d01f86d357ea07fb0aca9bdd58d8ce5f8487d0da
Dmitriy Kagarlickij <dmitriy@kagarlickij.com>
2022-09-30 15:50:17 +0200

    init

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "aws_iam_role" {
+  name = var.role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "arn" {
+  description = "ARN of the Role"
+  value       = aws_iam_role.aws_iam_role.arn
+}
